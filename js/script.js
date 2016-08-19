b104e16105ca385d2c8bfb138753e9a5cc5bcc3c
Tim Bourgault <tim.bourgault@gmail.com>
2016-08-19 14:07:35 -0700

    update user interface logic to include basic function input and output variables and for loop.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 //Business Logic:
 function pinkBong(desiredNumber) {
+  var list = []
+  for (var i = 1; i <= desiredNumber; i++) {
+    if (i % 15 === 0) {
+      list.push("PinkBong");
 
+    } else if (i % 3 === 0) {
+      list.push("Pink");
+
+    } else if (i % 5 === 0) {
+      list.push("Bong");
+
+    } else {
+      list.push(i);
+    }
+  }
+  return list
 }
 //User Interface Logic:
 $(document).ready(function() {
-  $("form#input")
+  $("form#input").submit(function(event) {
+
+    event.preventDefault();
+    var desiredInput = $("input#desiredNumber").val();
+    var result = pinkBong(desiredInput)
+
+  for (x = 0; x < desiredInput; x++)
+  }
 })
