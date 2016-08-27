1066609b628a964f925063990d566f9124a8b474
Tim <tim.bourgault@gmail.com>
2016-08-26 18:02:05 -0700

    solve js bugs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,31 @@
 //Business Logic:
+
 function pinkBong(num) {
-  var list = []
+  var list = [];
   for (var i = 1; i <= num; i++) {
 
     if (i % 15 === 0) {
       list.push("PinkBong");
-
     } else if (i % 5 === 0) {
       list.push("Bong");
-
     } else if (i % 3 === 0) {
       list.push("Pink");
-
     } else {
       list.push(i);
     }
   }
-  return list
+  return list;
 }
-//User Interface Logic:
+// //User Interface Logic:
 $(document).ready(function() {
-  $("form#input").submit(function(event) {
-    var input = $("input#num").val();
-  $("form#button").click(function(event) {
-    var result = pinkBong(input);
-  })
-  for (var j = 0; j < input; j++);
-    $("#result").append("<li>" + result[j] + "</li>");
-  })
-  $("input#number").keydown(function() {
-    $("#result").text("");
-    $(".output").show();
+  $("form").submit(function(event) {
     event.preventDefault();
+    var input = parseInt($("input#num").val());
+    var result = [];
+    result = pinkBong(input);
+    for (var j = 0; j < result.length; j++) {
+    $("ul#result").append("<li>" + result[j] + "</li>");
+    };
+    $(".output").show();
   });
 });
